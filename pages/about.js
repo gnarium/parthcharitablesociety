98066615048906038f11f79be162118cda3191c5
gnarium <80353633+gnarium@users.noreply.github.com>
2021-05-14 00:43:04 +0530

    end

--- pages/about.js
+++ pages/about.js
@@ -53,200 +53,22 @@ export default function About()
             </div> 
         </div> 
     </section>
-    <section className="services_area_2 pt-130 pb-130 bg_cover" style={{backgroundImage: 'url(assets/images/gray-bg.jpg)'}}>
-        <div className="services_shape_1" style={{backgroundImage: 'url(assets/images/shape/shape-1.png)'}}></div>
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="section_title text-center pb-30">
-                        <img src="assets/images/section_icon.png" alt="Icon"/>
-                        <h3 className="title">Our Activities</h3>
-                    </div>
-                </div>
-            </div> 
-            <div className="row justify-content-center">
-                <div className="col-lg-4 col-md-7">
-                    <div className="single_services_2 text-center mt-30 services_2_color_1">
-                        <div className="services_image">
-                            <a href="#"><img src="assets/images/help-1.jpg" alt="help"/></a>
-                        </div>
-                        <div className="services_content">
-                            <h4 className="services_title"><a href="#">Be a Volunteer</a></h4>
-                            <p>Logo ka samajik , mansik, natik ,charitrik, adhatmaya, saririk, bodhik vikas karna.</p>
-                        </div>
-                    </div> 
-                </div>
-                <div className="col-lg-4 col-md-7">
-                    <div className="single_services_2 text-center mt-30 services_2_color_2">
-                        <div className="services_image">
-                            <a href="#"><img src="assets/images/help-2.jpg" alt="help"/></a>
-                        </div>
-                        <div className="services_content">
-                            <h4 className="services_title"><a href="#">Donate Now</a></h4>
-                            <p>Balak and balika ki nishiuk shiksha ki vaysatha karna.</p>
-                        </div>
-                    </div> 
-                </div>
-                <div className="col-lg-4 col-md-7">
-                    <div className="single_services_2 text-center mt-30 services_2_color_2">
-                        <div className="services_image">
-                            <a href="#"><img src="assets/images/help-2.jpg" alt="help"/></a>
-                        </div>
-                        <div className="services_content">
-                            <h4 className="services_title"><a href="#">Donate Now</a></h4>
-                            <p style={{textAlign:'justify'}}>silai, kadhai, bunai, beauty parlor , mehandi prashikshad , fashion designing , redimat garments prashikshad.</p>
-                        </div>
-                    </div> 
-                </div>
-                <div className="col-lg-4 col-md-7">
-                    <div className="single_services_2 text-center mt-30 services_2_color_3">
-                        <div className="services_image">
-                            <a href="#"><img src="assets/images/help-3.jpg" alt="help"/></a>
-                        </div>
-                        <div className="services_content">
-                            <h4 className="services_title"><a href="#">Send Gift</a></h4>
-                            <p>swasth ke prati jagruk karna.</p>
-                        </div>
-                    </div> 
-                </div>
-                <div className="col-lg-4 col-md-7">
-                    <div className="single_services_2 text-center mt-30 services_2_color_2">
-                        <div className="services_image">
-                            <a href="#"><img src="assets/images/help-2.jpg" alt="help"/></a>
-                        </div>
-                        <div className="services_content">
-                            <h4 className="services_title"><a href="#">Donate Now</a></h4>
-                            <p style={{textAlign:'justify'}}>Samaj mai shoshit,besahara aur kamjoor varg ki mahilao aur baccho mai samajik , shaksadhik , aarthik uttan ke liye samarpit sanstha.</p>
-                        </div>
-                    </div> 
-                </div>
-                
-            </div> 
-        </div> 
-        <div className="services_shape_2" style={{backgroundImage: 'url(assets/images/shape/shape-2.png)'}}></div>
-    </section>
-
-    <section className="video_area_3 pt-80 pb-130 bg_cover" style={{backgroundImage: 'url(assets/images/videobg.jpg)'}}>
+    <section className="video_area_3 pt-80 pb-130 bg_cover" style={{backgroundImage: 'url(assets/images/4.jpg)'}}>
         <div className="container">
             <div className="row align-items-center">
-                <div className="col-lg-6">
-                    <div className="video_image mt-50">
-                        <img src="assets/images/video-image.jpg" alt="video"/>
-                        <a href="https://www.youtube.com/watch?v=WKUjCCOcLFY" className="video-popup">
-                            <i className="fa fa-play"></i>
-                            <img src="assets/images/shape/shape-3.png" alt="shape"/>
-                        </a>
-                    </div> 
+                <div className="col-lg-7">
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-5">
                     <div className="video_content_3 mt-45">
-                        <h4 className="video_title">We served more then 80k+ peoples</h4>
-                        <p>Helpgrove is a nonproﬁt organization supported by community leaders, corporate sponsors, churches and concerned citizens join. There will be a day–in our lifetime–when we get to celebrate.</p>
+                        <h4 className="video_title">We served more then 50k+ peoples</h4>
+                        <p>Parth Charitable Society is a nonproﬁt organization supported by community leaders, corporate sponsors, churches and concerned citizens join. There will be a day–in our lifetime–when we get to celebrate.</p>
                         <a href="#" className="main-btn"><i className="fa fa-heart"></i> Donate  Now</a>
                     </div> 
                 </div>
             </div> 
         </div> 
     </section>
-    <section className="volunteer_area pt-130 pb-130">
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="section_title text-center pb-30">
-                        <img src="assets/images/section_icon.png" alt="Icon"/>
-                        <h3 className="title">Our Volunteers</h3>
-                    </div> 
-                </div>
-            </div> 
-            <div className="row">
-                <div className="col-lg-3 col-sm-6">
-                    <div className="single_volunteer mt-30 volunteer_color_1">
-                        <div className="volunteer_image">
-                            <img src="assets/images/volunteer-1.jpg" alt="Volunteer"/>
-                            <div className="volunteer_social">
-                                <ul className="social">
-                                    <li><a href="#"><i className="fa fa-facebook-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-twitter-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-linkedin-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-                                </ul>
-                            </div> 
-                        </div>
-                        <div className="volunteer_content d-flex align-items-center justify-content-center">
-                            <div className="content_wrapper">
-                                <h5 className="volunteer_name"><a href="#">Maria Noor</a></h5>
-                                <p>Volunteer</p>
-                            </div>
-                        </div>
-                    </div> 
-                </div>
-                <div className="col-lg-3 col-sm-6">
-                    <div className="single_volunteer mt-30 volunteer_color_2">
-                        <div className="volunteer_image">
-                            <img src="assets/images/volunteer-2.jpg" alt="Volunteer"/>
-                            <div className="volunteer_social">
-                                <ul className="social">
-                                    <li><a href="#"><i className="fa fa-facebook-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-twitter-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-linkedin-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-                                </ul>
-                            </div> 
-                        </div>
-                        <div className="volunteer_content d-flex align-items-center justify-content-center">
-                            <div className="content_wrapper">
-                                <h5 className="volunteer_name"><a href="#">Kiara Roy</a></h5>
-                                <p>Volunteer</p>
-                            </div>
-                        </div>
-                    </div> 
-                </div>
-                <div className="col-lg-3 col-sm-6">
-                    <div className="single_volunteer mt-30 volunteer_color_3">
-                        <div className="volunteer_image">
-                            <img src="assets/images/volunteer-3.jpg" alt="Volunteer"/>
-                            <div className="volunteer_social">
-                                <ul className="social">
-                                    <li><a href="#"><i className="fa fa-facebook-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-twitter-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-linkedin-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-                                </ul>
-                            </div> 
-                        </div>
-                        <div className="volunteer_content d-flex align-items-center justify-content-center">
-                            <div className="content_wrapper">
-                                <h5 className="volunteer_name"><a href="#">Adams Hobes</a></h5>
-                                <p>Volunteer</p>
-                            </div>
-                        </div>
-                    </div> 
-                </div>
-                <div className="col-lg-3 col-sm-6">
-                    <div className="single_volunteer mt-30 volunteer_color_4">
-                        <div className="volunteer_image">
-                            <img src="assets/images/donor.jpeg" alt="Volunteer"/>
-                            <div className="volunteer_social">
-                                <ul className="social">
-                                    <li><a href="#"><i className="fa fa-facebook-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-twitter-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-linkedin-square"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-                                </ul>
-                            </div> 
-                        </div>
-                        <div className="volunteer_content d-flex align-items-center justify-content-center">
-                            <div className="content_wrapper">
-                                <h5 className="volunteer_name"><a href="#">Adams Hobes</a></h5>
-                                <p>Volunteer</p>
-                            </div>
-                        </div>
-                    </div> 
-                </div>
-            </div> 
-        </div> 
-    </section>
-    
+    &nbsp;
         </div>
     )
 }
