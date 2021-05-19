7e92dbd1bda71d432289dbccb8e5627a1146940f
gnarium <80353633+gnarium@users.noreply.github.com>
2021-05-19 21:56:30 +0530

    end

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
-import Popup from 'reactjs-popup';
+
 import  { useRef } from 'react';
 const Header = () => {
-  const ref = useRef();
-  const openTooltip = () => ref.current.open();
-  const closeTooltip = () => ref.current.close();
-  const toggleTooltip = () => ref.current.toggle();
     return(
         <div>
                 <header className="header_area">
@@ -73,23 +69,7 @@ const Header = () => {
 
                     <div className="navbar_btn d-none d-sm-block">
                               
-      <Popup
-        ref={ref}
-        trigger={
-          <button type="button" className="btn btn-danger">
-            Donate Now
-          </button>
-        }
-      >
-        <div>
-            <b>Account Number:</b>38183233025
-
-        </div>
-        <div>
-            <b>IFSC Code:</b>SBIN0012830
-            
-        </div>
-      </Popup>
+      
                     </div>
                 
                 </nav> 
