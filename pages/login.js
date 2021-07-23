c873e1d116b945ab2cbd462a7fe69ec1313d8c28
gnarium <80353633+gnarium@users.noreply.github.com>
2021-07-23 22:53:16 +0530

    dd

--- pages/login.js
+++ pages/login.js
@@ -11,7 +11,7 @@ const Login = () =>{
     const userLogin = async (e)=>
     {
         e.preventDefault()
-        fetch(`${baseUrl}/api/login`,{
+        fetch(`https://www.parthcharitablesociety.org/api/login`,{
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
